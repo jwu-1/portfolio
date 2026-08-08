85629a52f3e637925eb2e2bfa5ecac387e00a1d0
Justin Wu <justin.m.wu@protonmail.com>
2026-08-08 19:28:23 -0400

    installed new react and updated accordingly

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
